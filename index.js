9d083233f9b73ffeaf163d378a695be6b300cb59
Jade <syndamagaming@gmail.com>
2021-06-13 17:33:23 +1000

    Serve Homepage

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const ejs = require('ejs');
+
+app = express();
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/css'));
+
+app.get(['/', '/home'], (req, res) => {
+    res.render('index');
+});
+
+app.listen(443);
+console.log("We're live on http://localhost:443");
