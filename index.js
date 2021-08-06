0dbf4dd1d07c851456dbcb120e3786f07d0f8094
Jade <syndamagaming@gmail.com>
2021-08-06 22:02:23 +1000

    Disable video server functionality.

--- index.js
+++ index.js
@@ -22,54 +22,54 @@ const connection = mysql.createConnection({
 
 const io = new Server(server);
 
-const insertVideoSQL = function(videoName, videoType, videoPath) {
-    const sql = 'INSERT INTO videos (videoName, videoType, videoPath) VALUES ("' + videoName + '", "' + videoType + '", "' + videoPath + '")';
-    return sql;
-};
+// Code for programming building a video library, using SQL, but needs to be fixed with proper video reference indexing. Also want to move to it's own module.
 
-const getAllVideoFiles = function(dirPath, arrayOfFiles) {
-    files = fs.readdirSync(dirPath);
+// const insertVideoSQL = function(videoName, videoType, videoPath) {
+//     const sql = 'INSERT INTO videos (videoName, videoType, videoPath) VALUES ("' + videoName + '", "' + videoType + '", "' + videoPath + '")';
+//     return sql;
+// };
+
+// const getAllVideoFiles = function(dirPath, arrayOfFiles) {
+//     files = fs.readdirSync(dirPath);
   
-    arrayOfFiles = arrayOfFiles || [];
+//     arrayOfFiles = arrayOfFiles || [];
   
-    files.forEach(function(file) {
-        if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-            arrayOfFiles = getAllVideoFiles(dirPath + "/" + file, arrayOfFiles);
-        } else {
-            fileName = file;
-            fileType = path.extname(file).replace('.', '');
-            switch (path.extname(file)) {
-                case '.mp4':
-                    arrayOfFiles.push({name: fileName, type: fileType, path: dirPath});
-            }
-        }
-    });
+//     files.forEach(function(file) {
+//         if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+//             arrayOfFiles = getAllVideoFiles(dirPath + "/" + file, arrayOfFiles);
+//         } else {
+//             fileName = file;
+//             fileType = path.extname(file).replace('.', '');
+//             switch (path.extname(file)) {
+//                 case '.mp4':
+//                     arrayOfFiles.push({name: fileName, type: fileType, path: dirPath});
+//             }
+//         }
+//     });
   
-    return arrayOfFiles;
-};
+//     return arrayOfFiles;
+// };
 
-videoList = getAllVideoFiles(sqlConfig['content-library']);
+// videoList = getAllVideoFiles(sqlConfig['content-library']);
 
-const pushToSQL = function(array) {
-    connection.query("DELETE FROM videos", (err) => {
-        if (err) null;
-    });
-
-    array.forEach(element => {
-        connection.query(insertVideoSQL(element.name, element.type, element.path), (err) => {
-            if (err) null;
-        });
-    });
-};
+// const pushToSQL = function(array) {
+//     array.forEach(element => {
+//         connection.query(insertVideoSQL(element.name, element.type, element.path), (err) => {
+//             if (err) {
+//                 if (err.errno == 1062) null;
+//             }
+//         });
+//     });
+// };
 
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log("Connected to SQL Database!");
+// connection.connect((err) => {
+//     if (err) throw err;
+//     console.log("Connected to SQL Database!");
 
-    pushToSQL(videoList);
+//     pushToSQL(videoList);
     
-});
+// });
 
 app.set('view engine', 'ejs');
 app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'views/gallery')]);
@@ -83,52 +83,55 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/gallery', (req, res) => {
-    res.render('gallery');
-});
 
-app.get('/gallery/view', (req, res) => {
-    videoID = req.query.q;
-    if (!videoID) {
-        res.redirect('/gallery');
-        throw null;
-    };
+// Code for programming building a video library, using SQL, but needs to be fixed with proper video reference indexing. Also want to move to it's own module.
 
-    connection.query("SELECT * FROM videos WHERE id = " + req.query.q, (err, result) => {
-        if (err) {
-            res.redirect('/gallery');
-            throw err;
-        };
+// app.get('/gallery', (req, res) => {
+//     res.render('gallery');
+// });
 
-        const videoPath = result[0].videoPath + '/' + result[0].videoName;
-        const videoType = result[0].videoType;
+// app.get('/gallery/view', (req, res) => {
+//     videoID = req.query.q;
+//     if (!videoID) {
+//         res.redirect('/gallery');
+//         throw null;
+//     };
+
+//     connection.query("SELECT * FROM videos WHERE id = " + req.query.q, (err, result) => {
+//         if (err) {
+//             res.redirect('/gallery');
+//             throw err;
+//         };
+
+//         const videoPath = result[0].videoPath + '/' + result[0].videoName;
+//         const videoType = result[0].videoType;
         
-        var range = req.get("range");
-        if (!range) { range = `bytes 0-`; }
+//         var range = req.get("range");
+//         if (!range) { range = `bytes 0-`; }
 
         
-        const videoSize = fs.statSync(videoPath).size;
+//         const videoSize = fs.statSync(videoPath).size;
         
-        const CHUNK_SIZE = (10 ** 6) * 8; // 8MB
-        const start = Number(range.replace(/\D/g, ""));
-        const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+//         const CHUNK_SIZE = (10 ** 6) * 8; // 8MB
+//         const start = Number(range.replace(/\D/g, ""));
+//         const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
-        const contentLength = end - start + 1;
-        const headers = {
-            "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-            "Accept-Ranges": "bytes",
-            "Content-Length": contentLength,
-            "Content-Type": "video/" + videoType
-        };
+//         const contentLength = end - start + 1;
+//         const headers = {
+//             "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+//             "Accept-Ranges": "bytes",
+//             "Content-Length": contentLength,
+//             "Content-Type": "video/" + videoType
+//         };
 
-        res.writeHead(206, headers);
+//         res.writeHead(206, headers);
 
-        const videoStream = fs.createReadStream(videoPath, { start, end });
+//         const videoStream = fs.createReadStream(videoPath, { start, end });
 
-        videoStream.pipe(res);
-    });
+//         videoStream.pipe(res);
+//     });
 
-});
+// });
 
 app.get('/playlists', (req, res) => {
     res.render('playlists');
